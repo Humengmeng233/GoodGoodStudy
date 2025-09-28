178b8cd16fc2385fad64fd5dbbe4eda7789ccad8
HMM <humengmeng233@gmail.com>
2025-09-28 21:09:39 +0800

    🎉 init: 美丽的鸡

---
+++ docs/.vuepress/plume.config.ts
@@ -0,0 +1,85 @@
+/**
+ * 查看以下文档了解主题配置
+ * - @see https://theme-plume.vuejs.press/config/intro/ 配置说明
+ * - @see https://theme-plume.vuejs.press/config/theme/ 主题配置项
+ *
+ * 请注意，对此文件的修改不会重启 vuepress 服务，而是通过热更新的方式生效
+ * 但同时部分配置项不支持热更新，请查看文档说明
+ * 对于不支持热更新的配置项，请在 `.vuepress/config.ts` 文件中配置
+ *
+ * 特别的，请不要在两个配置文件中重复配置相同的项，当前文件的配置项会覆盖 `.vuepress/config.ts` 文件中的配置
+ */
+
+import type { ThemeConfig } from 'vuepress-theme-plume'
+import {defineThemeConfig} from 'vuepress-theme-plume'
+import {navbar} from './navbar'
+import {notes} from './notes'
+import * as path from "node:path";
+
+/**
+ * @see https://theme-plume.vuejs.press/config/basic/
+ */
+export default defineThemeConfig({
+    logo: '/android-chrome-512x512.png',
+
+    appearance: true,  // 配置 深色模式
+
+    social: [
+        {icon: 'github', link: 'https://github.com/Humengmeng233'},
+        {icon: 'qq', link: 'https://github.com/Humengmeng233'},
+    ],
+    navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
+    // aside: true, // 页内侧边栏， 默认显示在右侧
+    outline: [2, 3], // 页内大纲， 默认显示 h2, h3
+
+    /**
+     * 文章版权信息
+     * @see https://theme-plume.vuejs.press/guide/features/copyright/
+     */
+    copyright: true,
+
+    prevPage: true,   // 是否启用上一页链接
+    nextPage: true,   // 是否启用下一页链接
+    createTime: true, // 是否显示文章创建时间
+
+    /* 站点页脚 */
+    footer: {
+        message: 'Power by <a target="_blank" href="https://theme-plume.vuejs.press/">theme-plume</a> & <a target="_blank" href="https://github.com/caobaoqi6040">yuan-idea-docs</a>',
+        copyright: 'Copyright © Humengmeng233',
+    },
+
+    /**
+     * @see https://theme-plume.vuejs.press/config/basic/#profile
+     */
+    profile: {
+        avatar: '/avatar.png',
+        name: 'Humengmeng233',
+        description: 'good good study,day day up.',
+        circle: true,
+        location: 'ShanXi, China',
+        organization: 'ikun',
+    },
+
+    navbar,
+    notes,
+
+    /**
+     * 公告板
+     * @see https://theme-plume.vuejs.press/guide/features/bulletin/
+     */
+    bulletin: {
+        layout: 'top-right',
+        lifetime: 'always',
+        title: '🎉 消息通知 🎉',
+        contentFile: path.join(__dirname, 'bulletin.md'),
+        enablePage: page => page.path === '/',
+    },
+
+    /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
+    transition: {
+        page: true,        // 启用 页面间跳转过渡动画
+        postList: true,    // 启用 博客文章列表过渡动画
+        appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
+    },
+
+}) as ThemeConfig
---
+++ docs/README.md
@@ -0,0 +1,31 @@
+---
+pageLayout: home
+externalLinkIcon: false
+config:
+  -
+    type: hero
+    full: true
+    background: tint-plate
+    hero:
+      name: Humengmeng233
+      text: GoodGoodStudy 🌈
+      actions:
+        -
+          theme: brand
+          text: blog
+          link: /blog/
+        -
+          theme: alt
+          text: 笔记
+          link: /notes/
+  -
+    type: custom
+---
+
+
+## repo
+
+<CardGrid>
+  <RepoCard repo="Humengmeng233/GoodGoodStudy" provider="github"></RepoCard>
+</CardGrid>
+
